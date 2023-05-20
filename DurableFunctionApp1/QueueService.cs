05120939289267385109754d5ebeed82d776c334
Yuta Matsumura <tkmbmbr@gmail.com>
2023-05-21 07:24:59 +0900

    Add azure client

---
+++ DurableFunctionApp1/QueueService.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Storage.Queues;
+
+namespace DurableFunctionApp1;
+
+public class QueueService : IQueueService
+{
+    private readonly QueueClient queueClient;
+
+    public QueueService(QueueClient queueClient)
+    {
+        this.queueClient = queueClient;
+    }
+}
+
+public interface IQueueService
+{
+
+}
--- DurableFunctionApp1/Startup.cs
+++ DurableFunctionApp1/Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using Azure.Storage.Queues;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 [assembly: FunctionsStartup(typeof(DurableFunctionApp1.Startup))]
 namespace DurableFunctionApp1;
@@ -9,5 +13,19 @@ public class Startup : FunctionsStartup
     public override void Configure(IFunctionsHostBuilder builder)
     {
         builder.Services.AddHttpClient();
+
+        // https://stackoverflow.com/a/74421494
+        builder.Services.AddAzureClients(clientBuilder =>
+        {
+            clientBuilder.AddClient<QueueClient, QueueClientOptions>(options =>
+            {
+                IServiceProvider sp = builder.Services.BuildServiceProvider();
+                IConfiguration configuration = sp.GetRequiredService<IConfiguration>();
+                string connectionString = configuration["AzureWebJobsStorage"];
+                return new(connectionString, "myqueue-items");
+            });
+        });
+
+        builder.Services.AddSingleton<IQueueService, QueueService>();
     }
 }
